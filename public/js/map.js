ad8374c73ca9b20508db9c3b5d413f52e3666cc8
Lucas Scarlato Astur <lucassa3@al.insper.edu.br>
2016-12-03 16:08:54 -0200

    client screens

---
+++ public/js/map.js
@@ -0,0 +1,31 @@
+function isConstrained() {
+                return $(".sidebar").width() == $(window).width();
+            }
+
+            function applyInitialUIState() {
+                if (isConstrained()) {
+                    $(".sidebar-left .sidebar-body").fadeOut('slide');
+                    $('.mini-submenu-left').fadeIn();
+                }
+            }
+
+            $(function(){
+                $('.sidebar-left .slide-submenu').on('click',function() {
+                    var thisEl = $(this);
+                    thisEl.closest('.sidebar-body').fadeOut('slide',function(){
+                        $('.mini-submenu-left').fadeIn();
+                        applyMargins();
+                    });
+                });
+
+                $('.mini-submenu-left').on('click',function() {
+                    var thisEl = $(this);
+                    $('.sidebar-left .sidebar-body').toggle('slide');
+                    thisEl.hide();
+                    applyMargins();
+                });
+
+                $(window).on("resize", applyMargins);
+                    applyInitialUIState();
+                    applyMargins();
+                });
